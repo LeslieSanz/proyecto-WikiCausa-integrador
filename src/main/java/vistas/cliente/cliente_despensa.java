cee80c79fa76ba8f2305fb1ac8c5077d3aacead4
Yoss20 <118304980+Yoss20@users.noreply.github.com>
2024-06-12 01:33:39 -0500

    inicio terminado, despensa faltan en 80 %

--- src/main/java/vistas/cliente/cliente_despensa.java
+++ src/main/java/vistas/cliente/cliente_despensa.java
@@ -12,29 +12,94 @@ public class cliente_despensa extends javax.swing.JPanel {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        frutasVerduras = new javax.swing.JButton();
+        lacteos = new javax.swing.JButton();
+        abarrotes = new javax.swing.JButton();
+        carnes = new javax.swing.JButton();
+        pastas = new javax.swing.JButton();
+        congelados = new javax.swing.JButton();
+        jLabel10 = new javax.swing.JLabel();
+        QueCocinareHoy = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Poppins", 1, 48)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(170, 89, 19));
-        jLabel1.setText("Aquí va la despensa");
+        jLabel1.setText("Mi despensa");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 50, -1, -1));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(96, 96, 96)
-                .addComponent(jLabel1)
-                .addContainerGap(455, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(81, 81, 81)
-                .addComponent(jLabel1)
-                .addContainerGap(566, Short.MAX_VALUE))
-        );
+        frutasVerduras.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Despensa/frutasVerduras.png"))); // NOI18N
+        frutasVerduras.setContentAreaFilled(false);
+        jPanel1.add(frutasVerduras, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, -1, -1));
+
+        lacteos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Despensa/lacteos.png"))); // NOI18N
+        lacteos.setContentAreaFilled(false);
+        jPanel1.add(lacteos, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 130, -1, -1));
+
+        abarrotes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Despensa/abarrotes.png"))); // NOI18N
+        abarrotes.setContentAreaFilled(false);
+        jPanel1.add(abarrotes, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 130, -1, -1));
+
+        carnes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Despensa/carnes.png"))); // NOI18N
+        carnes.setToolTipText("");
+        carnes.setContentAreaFilled(false);
+        jPanel1.add(carnes, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 130, -1, -1));
+
+        pastas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Despensa/pastas.png"))); // NOI18N
+        pastas.setText(" ");
+        pastas.setContentAreaFilled(false);
+        jPanel1.add(pastas, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 130, -1, -1));
+
+        congelados.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Despensa/congelados.png"))); // NOI18N
+        congelados.setContentAreaFilled(false);
+        jPanel1.add(congelados, new org.netbeans.lib.awtextra.AbsoluteConstraints(870, 130, -1, -1));
+
+        jLabel10.setFont(new java.awt.Font("Poppins", 0, 18)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(46, 46, 46));
+        jLabel10.setText("Cantidad");
+        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 380, 100, -1));
+
+        QueCocinareHoy.setFont(new java.awt.Font("Poppins", 1, 24)); // NOI18N
+        QueCocinareHoy.setText("¿Qué tengo para cocinar?");
+        jPanel1.add(QueCocinareHoy, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 310, -1, -1));
+
+        jLabel11.setFont(new java.awt.Font("Poppins", 0, 15)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(46, 46, 46));
+        jLabel11.setText("¡Selecciona un producto y agrégalo a tu despensa!");
+        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 320, -1, -1));
+
+        jLabel12.setFont(new java.awt.Font("Poppins", 0, 18)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(46, 46, 46));
+        jLabel12.setText("Alimento");
+        jPanel1.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 380, 100, -1));
+
+        jLabel13.setFont(new java.awt.Font("Poppins", 0, 18)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(46, 46, 46));
+        jLabel13.setText("Cantidad");
+        jPanel1.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 380, 100, -1));
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_general/btnAzul.png"))); // NOI18N
+        jButton1.setContentAreaFilled(false);
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 560, -1, -1));
+
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_general/btnRojo.png"))); // NOI18N
+        jButton2.setContentAreaFilled(false);
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 560, -1, -1));
+
+        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_general/btnVerde.png"))); // NOI18N
+        jButton3.setContentAreaFilled(false);
+        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 480, -1, -1));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_general/Line 135.png"))); // NOI18N
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 320, 20, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -50,7 +115,25 @@ public class cliente_despensa extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel QueCocinareHoy;
+    private javax.swing.JButton abarrotes;
+    private javax.swing.JButton btnLogin;
+    private javax.swing.JButton btnLogin1;
+    private javax.swing.JButton btnLogin2;
+    private javax.swing.JButton carnes;
+    private javax.swing.JButton congelados;
+    private javax.swing.JButton frutasVerduras;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton lacteos;
+    private javax.swing.JButton pastas;
     // End of variables declaration//GEN-END:variables
 }
--- src/main/java/vistas/cliente/cliente_inicio.java
+++ src/main/java/vistas/cliente/cliente_inicio.java
@@ -11,30 +11,157 @@ public class cliente_inicio extends javax.swing.JPanel {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        fraseMotivadora = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        bannerPequeñoAmarillo = new javax.swing.JLabel();
+        QueCocinareHoy = new javax.swing.JLabel();
+        imagenAmigos = new javax.swing.JLabel();
+        panelAmarillo = new javax.swing.JLabel();
+        btnBebidas = new javax.swing.JButton();
+        btnHamburguesas = new javax.swing.JButton();
+        btnPollito = new javax.swing.JButton();
+        btnPostrecitos = new javax.swing.JButton();
+        panelDerechoTuDespensa = new javax.swing.JPanel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        Categorias = new javax.swing.JLabel();
+        bannerBlancoDerecha = new javax.swing.JLabel();
+        estofado = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        btnVerTodas2 = new javax.swing.JToggleButton();
+        jLabel10 = new javax.swing.JLabel();
+        sopa = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        btnVerTodas = new javax.swing.JToggleButton();
+        btnVerTodas1 = new javax.swing.JToggleButton();
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(245, 245, 245));
+        jPanel1.setMaximumSize(new java.awt.Dimension(32767, 32767));
+        jPanel1.setMinimumSize(new java.awt.Dimension(0, 0));
+        jPanel1.setPreferredSize(new java.awt.Dimension(1049, 720));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Poppins", 1, 48)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(170, 89, 19));
-        jLabel1.setText("Aquí va el inicio");
+        fraseMotivadora.setFont(new java.awt.Font("Poppins", 0, 15)); // NOI18N
+        fraseMotivadora.setForeground(new java.awt.Color(46, 46, 46));
+        fraseMotivadora.setText("Te quiero mucho <3");
+        jPanel1.add(fraseMotivadora, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 140, -1, -1));
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(96, 96, 96)
-                .addComponent(jLabel1)
-                .addContainerGap(567, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(81, 81, 81)
-                .addComponent(jLabel1)
-                .addContainerGap(566, Short.MAX_VALUE))
-        );
+        jLabel2.setFont(new java.awt.Font("Poppins", 1, 48)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(170, 89, 19));
+        jLabel2.setText("FECHA");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 60, -1, -1));
+
+        bannerPequeñoAmarillo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_inicio/bannerDerechaAmarillo.png"))); // NOI18N
+        jPanel1.add(bannerPequeñoAmarillo, new org.netbeans.lib.awtextra.AbsoluteConstraints(800, 500, -1, -1));
+
+        QueCocinareHoy.setFont(new java.awt.Font("Poppins", 1, 24)); // NOI18N
+        QueCocinareHoy.setText("¿Qué cocinaré hoy?");
+        jPanel1.add(QueCocinareHoy, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 230, -1, -1));
+
+        imagenAmigos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_inicio/grupo-amigos-preparando-comida-cocina 1.png"))); // NOI18N
+        jPanel1.add(imagenAmigos, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 10, 310, -1));
+
+        panelAmarillo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_inicio/panelAmarillo.png"))); // NOI18N
+        jPanel1.add(panelAmarillo, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
+
+        btnBebidas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Categorias/Bebidas.png"))); // NOI18N
+        btnBebidas.setContentAreaFilled(false);
+        btnBebidas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBebidasActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnBebidas, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 500, -1, -1));
+
+        btnHamburguesas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Categorias/Hamburguesas.png"))); // NOI18N
+        btnHamburguesas.setContentAreaFilled(false);
+        jPanel1.add(btnHamburguesas, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 500, -1, -1));
+
+        btnPollito.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Categorias/Pollito.png"))); // NOI18N
+        btnPollito.setContentAreaFilled(false);
+        jPanel1.add(btnPollito, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 500, -1, -1));
+
+        btnPostrecitos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Categorias/Postrecitos.png"))); // NOI18N
+        btnPostrecitos.setContentAreaFilled(false);
+        jPanel1.add(btnPostrecitos, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 500, -1, -1));
+
+        panelDerechoTuDespensa.setBackground(new java.awt.Color(255, 255, 255));
+        panelDerechoTuDespensa.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel8.setFont(new java.awt.Font("Poppins", 0, 24)); // NOI18N
+        jLabel8.setText("¡TU DESPENSA");
+        panelDerechoTuDespensa.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 170, -1));
+
+        jLabel9.setFont(new java.awt.Font("Poppins", 0, 24)); // NOI18N
+        jLabel9.setText("ESTÁ VACÍA!");
+        panelDerechoTuDespensa.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, 170, -1));
+
+        jPanel1.add(panelDerechoTuDespensa, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 260, 180, 140));
+
+        Categorias.setFont(new java.awt.Font("Poppins", 1, 24)); // NOI18N
+        Categorias.setText("Categorías");
+        jPanel1.add(Categorias, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 450, -1, -1));
+
+        bannerBlancoDerecha.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/img_inicio/menuDerecha.png"))); // NOI18N
+        jPanel1.add(bannerBlancoDerecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 30, -1, -1));
+
+        estofado.setBackground(new java.awt.Color(245, 245, 245));
+        estofado.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Comidas/estofadoDePollo.png"))); // NOI18N
+        estofado.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, -1, -1));
+
+        btnVerTodas2.setFont(new java.awt.Font("Poppins", 0, 12)); // NOI18N
+        btnVerTodas2.setForeground(new java.awt.Color(248, 182, 2));
+        btnVerTodas2.setText("Más detalles");
+        btnVerTodas2.setBorder(null);
+        btnVerTodas2.setContentAreaFilled(false);
+        btnVerTodas2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVerTodas2ActionPerformed(evt);
+            }
+        });
+        estofado.add(btnVerTodas2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 40, -1, -1));
+
+        jLabel10.setFont(new java.awt.Font("Poppins", 0, 15)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(46, 46, 46));
+        jLabel10.setText("Estofado de pollo");
+        estofado.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 10, -1, -1));
+
+        jPanel1.add(estofado, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 270, 340, 160));
+
+        sopa.setBackground(new java.awt.Color(245, 245, 245));
+        sopa.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Comidas/sopaDeFideos.png"))); // NOI18N
+        sopa.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 180, -1));
+
+        jLabel6.setFont(new java.awt.Font("Poppins", 0, 15)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(46, 46, 46));
+        jLabel6.setText("Sopa de fideos");
+        sopa.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 10, -1, -1));
+
+        btnVerTodas.setFont(new java.awt.Font("Poppins", 0, 12)); // NOI18N
+        btnVerTodas.setForeground(new java.awt.Color(248, 182, 2));
+        btnVerTodas.setText("Más detalles");
+        btnVerTodas.setBorder(null);
+        btnVerTodas.setContentAreaFilled(false);
+        btnVerTodas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnVerTodasActionPerformed(evt);
+            }
+        });
+        sopa.add(btnVerTodas, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 40, -1, -1));
+
+        jPanel1.add(sopa, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 270, 330, 160));
+
+        btnVerTodas1.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        btnVerTodas1.setForeground(new java.awt.Color(248, 182, 2));
+        btnVerTodas1.setText("Ver Todas    >");
+        btnVerTodas1.setBorder(null);
+        btnVerTodas1.setContentAreaFilled(false);
+        jPanel1.add(btnVerTodas1, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 460, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -48,9 +175,44 @@ public class cliente_inicio extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnBebidasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBebidasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnBebidasActionPerformed
+
+    private void btnVerTodasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVerTodasActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnVerTodasActionPerformed
+
+    private void btnVerTodas2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVerTodas2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnVerTodas2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel Categorias;
+    private javax.swing.JLabel QueCocinareHoy;
+    private javax.swing.JLabel bannerBlancoDerecha;
+    private javax.swing.JLabel bannerPequeñoAmarillo;
+    private javax.swing.JButton btnBebidas;
+    private javax.swing.JButton btnHamburguesas;
+    private javax.swing.JButton btnPollito;
+    private javax.swing.JButton btnPostrecitos;
+    private javax.swing.JToggleButton btnVerTodas;
+    private javax.swing.JToggleButton btnVerTodas1;
+    private javax.swing.JToggleButton btnVerTodas2;
+    private javax.swing.JPanel estofado;
+    private javax.swing.JLabel fraseMotivadora;
+    private javax.swing.JLabel imagenAmigos;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel panelAmarillo;
+    private javax.swing.JPanel panelDerechoTuDespensa;
+    private javax.swing.JPanel sopa;
     // End of variables declaration//GEN-END:variables
 }
