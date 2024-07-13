d958564de25698dae5831804f41b32d9f9769a75
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2024-07-12 19:22:41 -0500

    Interfaces del historial al 70%

--- src/main/java/vistas/cliente/cliente_generarMenu.java
+++ src/main/java/vistas/cliente/cliente_generarMenu.java
@@ -18,7 +18,7 @@ import modeloDAO.UsurarioDAO;
 import modeloDAO.historialDAO;
 
 
-public class cliente_verHistorialMenu extends javax.swing.JPanel {
+public class cliente_generarMenu extends javax.swing.JPanel {
     
     HistorialDTO h;
     historialDAO hd;
@@ -51,7 +51,7 @@ public class cliente_verHistorialMenu extends javax.swing.JPanel {
         tblRecetasNombres.setModel(modelo2);
     }
     
-    public cliente_verHistorialMenu(String dniob){
+    public cliente_generarMenu(String dniob){
         initComponents();
         establecerColumnas();
         establecerColumnas2();
@@ -83,16 +83,19 @@ public class cliente_verHistorialMenu extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblRecetasImagenes = new javax.swing.JTable();
-        btnMostrar = new javax.swing.JButton();
-        txtCodMenu = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         tblRecetasNombres = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
-        jComboBox1 = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
+        jdateInicio = new com.toedter.calendar.JDateChooser();
+        jLabel4 = new javax.swing.JLabel();
+        jdateInicio1 = new com.toedter.calendar.JDateChooser();
+        jLabel5 = new javax.swing.JLabel();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(245, 245, 245));
 
@@ -101,7 +104,7 @@ public class cliente_verHistorialMenu extends javax.swing.JPanel {
 
         jLabel1.setFont(new java.awt.Font("Poppins", 1, 48)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(170, 89, 19));
-        jLabel1.setText("Consulta tus menús");
+        jLabel1.setText("Genera tu menú semanal");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 60, -1, -1));
 
         tblRecetasImagenes.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
@@ -120,18 +123,6 @@ public class cliente_verHistorialMenu extends javax.swing.JPanel {
 
         jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 320, 920, 125));
 
-        btnMostrar.setText("Mostrar");
-        btnMostrar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnMostrarActionPerformed(evt);
-            }
-        });
-        jPanel1.add(btnMostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 140, -1, -1));
-        jPanel1.add(txtCodMenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 110, 113, 23));
-
-        jLabel2.setText("Id Menu ");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 90, -1, -1));
-
         tblRecetasNombres.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
         tblRecetasNombres.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -148,45 +139,50 @@ public class cliente_verHistorialMenu extends javax.swing.JPanel {
 
         jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 420, 920, 90));
 
-        jButton1.setText("Generar nuevo menú");
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 170, -1, -1));
-
         jPanel2.setBackground(new java.awt.Color(251, 243, 202));
-
-        jComboBox1.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel3.setFont(new java.awt.Font("Poppins Medium", 0, 14)); // NOI18N
-        jLabel3.setText("Semana");
+        jLabel3.setText("Desde");
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, 50, -1));
 
         jButton2.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
-        jButton2.setText("Ver");
+        jButton2.setText("Generar menú");
+        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 30, -1, -1));
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
-                .addContainerGap(43, Short.MAX_VALUE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2)
-                    .addComponent(jLabel3))
-                .addContainerGap(40, Short.MAX_VALUE))
-        );
+        jdateInicio.setForeground(new java.awt.Color(153, 153, 153));
+        jdateInicio.setFont(new java.awt.Font("Poppins", 2, 14)); // NOI18N
+        jPanel2.add(jdateInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 30, 112, 30));
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 170, 350, -1));
+        jLabel4.setFont(new java.awt.Font("Poppins Medium", 0, 14)); // NOI18N
+        jLabel4.setText("Hasta");
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 30, 50, -1));
+
+        jdateInicio1.setForeground(new java.awt.Color(153, 153, 153));
+        jdateInicio1.setFont(new java.awt.Font("Poppins", 2, 14)); // NOI18N
+        jPanel2.add(jdateInicio1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 30, 112, 30));
+
+        jLabel5.setFont(new java.awt.Font("Poppins Medium", 0, 14)); // NOI18N
+        jLabel5.setText("Según");
+        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 30, 50, -1));
+
+        jRadioButton1.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        jRadioButton1.setText("Optimización de despensa");
+        jPanel2.add(jRadioButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 20, -1, -1));
+
+        jRadioButton2.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        jRadioButton2.setText("Preferencias");
+        jPanel2.add(jRadioButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 50, -1, -1));
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 170, 910, 100));
+
+        jButton3.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        jButton3.setText("Re-roll :p");
+        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 540, 150, -1));
+
+        jButton4.setFont(new java.awt.Font("Poppins", 0, 14)); // NOI18N
+        jButton4.setText("Confirmar");
+        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 630, 150, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -199,13 +195,6 @@ public class cliente_verHistorialMenu extends javax.swing.JPanel {
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnMostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMostrarActionPerformed
-        codMenu = txtCodMenu.getText();
-       mostrarMenuSemanal(codMenu);
-       mostrarNombresRecetas(codMenu);
-        
-    }//GEN-LAST:event_btnMostrarActionPerformed
 
     public void prueba(){
         System.out.println(dni);
@@ -353,19 +342,22 @@ public void mostrarMenuSemanal(String codMenu) {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnMostrar;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private com.toedter.calendar.JDateChooser jdateInicio;
+    private com.toedter.calendar.JDateChooser jdateInicio1;
     private javax.swing.JTable tblRecetasImagenes;
     private javax.swing.JTable tblRecetasNombres;
-    private javax.swing.JTextField txtCodMenu;
     // End of variables declaration//GEN-END:variables
 }
